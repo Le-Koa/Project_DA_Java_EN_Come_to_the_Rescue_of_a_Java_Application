0807187dcb54e0b4f9ddc686741b271d9b0b1d5b
Bennour Ilyass <ilyass.nourb@gmail.com>
2021-06-10 15:44:29 +0200

    Rectification du fichier launcher

--- Project02Eclipse/src/com/hemebiotech/analytics/Launcher.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Launcher.java
@@ -1,5 +1,26 @@
 package com.hemebiotech.analytics;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.TreeMap;
+
 public class Launcher {
 
+	public static void main(String[] args) throws IOException {
+		
+		//INSTANCIATION DE L'OBJET ANALYTICSCOUNTER
+		AnalyticsCounter analyticsCounter = new AnalyticsCounter();
+		
+		//Setup a list from the symptoms file
+		List<String> symptoms = analyticsCounter.getSymptoms();
+		
+		//Count the occurrences of a list and put it in a TreeMap
+		TreeMap<String, Integer> counting = analyticsCounter.getSymptomsCount(symptoms);
+		
+		//Put all occurrences in a output file
+		analyticsCounter.WriteSymptomFile(counting);
+		
+
+	}
 }
+
